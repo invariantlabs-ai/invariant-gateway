4d26a8b139ce5286c8907997d7075a2c5e3ea259
Hemang <hemang@invariantlabs.ai>
2025-01-29 22:24:21 +0100

    Update to make the proxy service work with existing Traeffik config of explorer.

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+
 # if DEV_MODE is true, then run the app with auto-reload
 if [ "$DEV_MODE" = "true" ]; then
     uvicorn serve:proxy_app --host 0.0.0.0 --port 8000 --reload
--- proxy/serve.py
+++ proxy/serve.py
@@ -1,6 +1,7 @@
 """Serve the API"""
 
 import fastapi
+import uvicorn
 from proxy import proxy
 
 v1 = fastapi.FastAPI()
@@ -8,20 +9,10 @@ v1 = fastapi.FastAPI()
 # install the API routes
 v1.mount("/proxy", proxy)
 
-
-# for debugging, we can check if the API is up
-@v1.get("/")
-async def home():
-    """Check if the API is up"""
-    return {"message": "Hello v1 proxy"}
-
-
 # mount the API under /api/v1
 proxy_app = fastapi.FastAPI()
 proxy_app.mount("/api/v1", v1)
 
 # serve the API
 if __name__ == "__main__":
-    import uvicorn
-
     uvicorn.run(proxy_app, host="0.0.0.0", port=8000)
