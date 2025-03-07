39b91e642988baf3aaa22e3d1a425ce0f70640ae
Hemang <hemang@invariantlabs.ai>
2025-03-07 10:00:01 +0100

    Add a converter module to house conversion from different LLM provider formats to Invariant API format.

--- tests/util.py
+++ tests/util.py
@@ -8,6 +8,7 @@ from playwright.async_api import async_playwright
 
 @pytest.fixture
 def gateway_url():
+    """Get the gateway URL from the environment variable"""
     if "INVARIANT_GATEWAY_API_URL" in os.environ:
         return os.environ["INVARIANT_GATEWAY_API_URL"]
     raise ValueError("Please set the INVARIANT_GATEWAY_API_URL environment variable")
@@ -15,6 +16,7 @@ def gateway_url():
 
 @pytest.fixture
 def explorer_api_url():
+    """Get the explorer API URL from the environment variable"""
     if "INVARIANT_API_URL" in os.environ:
         return os.environ["INVARIANT_API_URL"]
     raise ValueError("Please set the INVARIANT_API_URL environment variable")
@@ -22,19 +24,22 @@ def explorer_api_url():
 
 @pytest.fixture
 async def playwright(scope="session"):
+    """Fixture to create a Playwright instance"""
     async with async_playwright() as playwright_instance:
         yield playwright_instance
 
 
 @pytest.fixture
 async def browser(playwright, scope="session"):
-    browser = await playwright.firefox.launch(headless=True)
-    yield browser
-    await browser.close()
+    """Fixture to create a browser instance"""
+    firefox_browser = await playwright.firefox.launch(headless=True)
+    yield firefox_browser
+    await firefox_browser.close()
 
 
 @pytest.fixture
 async def context(browser):
-    context = await browser.new_context(ignore_https_errors=True)
-    yield context
-    await context.close()
+    """Fixture to create a browser context"""
+    browser_context = await browser.new_context(ignore_https_errors=True)
+    yield browser_context
+    await browser_context.close()
