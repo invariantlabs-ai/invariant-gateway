20febb69b3e964464de852de91080cce6d286427
Luca Beurer-Kellner <lucabeurerkellner@gmail.com>
2025-03-27 13:10:15 +0100

    initial draft: pipelined guardrails

--- gateway/run.sh
+++ gateway/run.sh
@@ -15,7 +15,7 @@ UVICORN_PORT=${PORT:-8000}
 # using 'exec' belows ensures that signals like SIGTERM are passed to the child process
 # and not the shell script itself (important when running in a container)
 if [ "$DEV_MODE" = "true" ]; then
-    exec uvicorn serve:app --host 0.0.0.0 --port $UVICORN_PORT --reload
+    exec uvicorn serve:app --host 0.0.0.0 --port $UVICORN_PORT --reload --reload-dir /srv/resources --reload-dir /srv/gateway
 else
     exec uvicorn serve:app --host 0.0.0.0 --port $UVICORN_PORT
 fi
