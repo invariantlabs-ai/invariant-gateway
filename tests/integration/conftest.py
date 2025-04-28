4b6a1982a85d5a63bee937eae410c389eddfd8e1
Hemang <hemang@invariantlabs.ai>
2025-04-28 12:37:57 +0530

    Add tests for MCP integration in gateway.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -4,6 +4,7 @@ import os
 
 import pytest
 
+
 @pytest.fixture
 def gateway_url():
     """Get the gateway URL from the environment variable"""
@@ -18,3 +19,17 @@ def explorer_api_url():
     if "INVARIANT_API_URL" in os.environ:
         return os.environ["INVARIANT_API_URL"]
     raise ValueError("Please set the INVARIANT_API_URL environment variable")
+
+
+@pytest.fixture
+def invariant_gateway_package_whl_file():
+    """Get the Invariant Gateway package wheel file"""
+    whl_file = None
+    for filename in os.listdir("/package"):
+        if filename.endswith(".whl") and "invariant_gateway" in filename:
+            whl_file = filename
+            break
+
+    if whl_file:
+        return f"/package/{whl_file}"
+    raise ValueError("No Invariant Gateway wheel file found in /package")
---
+++ tests/integration/resources/mcp/messenger_server/main.py
@@ -0,0 +1,43 @@
+"""This is a messenger server implementation that returns a few messages based on the username."""
+
+import random
+import hashlib
+
+from mcp.server.fastmcp import FastMCP
+
+# Initialize FastMCP server
+mcp = FastMCP("messenger_server")
+
+
+MESSAGES = [
+    "What about you?",
+    "What are you doing?",
+    "What is your name?",
+    "What is your favorite color?",
+    "What is your favorite food?",
+    "What is your favorite movie?",
+    "What is your favorite book?",
+]
+
+
+def _deterministic_index_from_username(username: str, limit: int) -> int:
+    """Deterministically calculate the index of messages to return based on the username."""
+    hash_val = int(hashlib.sha256(username.encode()).hexdigest(), 16)
+    return hash_val % limit + 1
+
+
+@mcp.tool()
+async def get_last_message_from_user(username: str) -> str:
+    """Get the last message sent by the username."""
+    return MESSAGES[_deterministic_index_from_username(username, len(MESSAGES))] + "\n"
+
+
+@mcp.tool()
+async def send_message(username: str, message: str) -> str:
+    """Send a message to the username."""
+    return f"Message '{message}' sent to {username}."
+
+
+if __name__ == "__main__":
+    # Initialize and run the server
+    mcp.run(transport="stdio")
