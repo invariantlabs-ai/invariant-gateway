cc3e96c20a6141db4286f4008751d27a1f8d4c7a
Hemang <hemang@invariantlabs.ai>
2025-06-04 10:07:22 +0200

    Clean up MCP tests and clients.

--- tests/integration/resources/mcp/stdio/client/main.py
+++ tests/integration/resources/mcp/stdio/client/main.py
@@ -1,106 +1,55 @@
 """A MCP client implementation that interacts with MCP server to make tool calls."""
 
-import asyncio
 import os
 
 from datetime import timedelta
-from contextlib import AsyncExitStack
 from typing import Any, Optional
 
 from mcp import ClientSession, StdioServerParameters, types
 from mcp.client.stdio import stdio_client
 
 
-class MCPClient:
-    """MCP Client for interacting with a MCP stdio server and processing queries"""
+def _get_server_params(
+    invariant_gateway_package_whl_file: str,
+    project_name: str,
+    server_script_path: str,
+    push_to_explorer: bool,
+    metadata_keys: Optional[dict[str, str]] = None,
+) -> StdioServerParameters:
+    args = [
+        "--from",
+        invariant_gateway_package_whl_file,
+        "invariant-gateway",
+        "mcp",
+        "--project-name",
+        project_name,
+    ]
+    # add metadata cli args
+    if metadata_keys is not None:
+        for key, value in metadata_keys.items():
+            args.append("--metadata-" + key + "=" + value)
 
-    def __init__(self):
-        self.session: Optional[ClientSession] = None
-        self.exit_stack = AsyncExitStack()
-
-    async def connect_to_server(
-        self,
-        invariant_gateway_package_whl_file: str,
-        project_name: str,
-        server_script_path: str,
-        push_to_explorer: bool,
-        metadata_keys: Optional[dict[str, str]] = None,
-    ):
-        """
-        Connect to an MCP server.
-
-        Args:
-            invariant_gateway_package_whl_file: Path to the Invariant Gateway package
-                .whl file
-            project_name: Name of the project in Invariant Explorer
-            server_script_path: Path to the server script
-            push_to_explorer: Whether to push traces to the Invariant Explorer
-        """
-        args = [
-            "--from",
-            invariant_gateway_package_whl_file,
-            "invariant-gateway",
-            "mcp",
-            "--project-name",
-            project_name,
+    if push_to_explorer:
+        args.append("--push-explorer")
+    args.extend(
+        [
+            "--exec",
+            "uv",
+            "--directory",
+            os.path.abspath(os.path.dirname(server_script_path)),
+            "run",
+            os.path.basename(server_script_path),
         ]
-        # add metadata cli args
-        if metadata_keys is not None:
-            for key, value in metadata_keys.items():
-                args.append("--metadata-" + key + "=" + value)
+    )
 
-        if push_to_explorer:
-            args.append("--push-explorer")
-        args.extend(
-            [
-                "--exec",
-                "uv",
-                "--directory",
-                os.path.abspath(os.path.dirname(server_script_path)),
-                "run",
-                os.path.basename(server_script_path),
-            ]
-        )
-
-        server_params = StdioServerParameters(
-            command="uvx",
-            args=args,
-            env={
-                "INVARIANT_API_KEY": os.environ.get("INVARIANT_API_KEY"),
-                "INVARIANT_API_URL": "http://invariant-gateway-test-explorer-app-api:8000",
-            },
-        )
-
-        stdio_transport = await self.exit_stack.enter_async_context(
-            stdio_client(server_params)
-        )
-        self.stdio, self.write = stdio_transport
-        self.session = await self.exit_stack.enter_async_context(
-            ClientSession(
-                self.stdio, self.write, read_timeout_seconds=timedelta(seconds=15)
-            )
-        )
-
-        # initialize the session
-        await self.session.initialize()
-
-    async def call_tool(
-        self, tool_name: str, tool_args: dict[str, Any]
-    ) -> types.CallToolResult:
-        """
-        Make a tool call on the MCP server.
-
-        Args:
-            tool_name: Name of the tool to call
-            tool_args: Arguments for the tool call
-        """
-        # Execute tool call
-        result = await self.session.call_tool(tool_name, tool_args)
-        return result
-
-    async def cleanup(self):
-        """Clean up resources"""
-        await self.exit_stack.aclose()
+    return StdioServerParameters(
+        command="uvx",
+        args=args,
+        env={
+            "INVARIANT_API_KEY": os.environ.get("INVARIANT_API_KEY"),
+            "INVARIANT_API_URL": os.environ.get("INVARIANT_API_URL"),
+        },
+    )
 
 
 async def run(
@@ -111,7 +60,7 @@ async def run(
     tool_name: str,
     tool_args: dict[str, Any],
     metadata_keys: Optional[dict[str, str]] = None,
-) -> types.CallToolResult:
+) -> types.CallToolResult | types.ListToolsResult:
     """
     Main function to setup the MCP client and server.
     It calls a tool on the server with the given args.
@@ -124,24 +73,26 @@ async def run(
         push_to_explorer: Whether to push traces to the Invariant Explorer
         tool_name: Name of the tool to call
         tool_args: Arguments for the tool call
+        metadata_keys: Optional metadata keys to include in the request
     """
-
-    client = MCPClient()
-    try:
-        await client.connect_to_server(
+    client = stdio_client(
+        _get_server_params(
             invariant_gateway_package_whl_file,
             project_name,
             server_script_path,
             push_to_explorer,
             metadata_keys=metadata_keys,
         )
-        listed_tools = await client.session.list_tools()
-        if tool_name == "tools/list":
+    )
+    async with client as (stdio, write):
+        async with ClientSession(
+            stdio, write, read_timeout_seconds=timedelta(seconds=10)
+        ) as session:
+            await session.initialize()
             # list tools
-            return listed_tools
-        else:
-            return await client.call_tool(tool_name, tool_args)
-    finally:
-        if push_to_explorer:
-            await asyncio.sleep(2)
-        await client.cleanup()
+            listed_tools = await session.list_tools()
+            # call tool
+            if tool_name == "tools/list":
+                return listed_tools
+            else:
+                return await session.call_tool(tool_name, tool_args)
